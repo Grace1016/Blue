81fc77fb3ac29caaa19f49d74598ed04266d84c3
王红叶 <wanghongye@wanghongyedeMacBook-Pro.local>
2019-12-11 22:58:59 +0800

    push proposal

---
+++ Main_Project/proposal/Compilelatex.sh
@@ -0,0 +1,16 @@
+pdflatex proposal.tex
+pdflatex proposal.tex
+bibtex proposal
+pdflatex proposal.tex
+pdflatex proposal.tex
+evince proposal.pdf &
+
+## Cleanup
+rm *~
+rm *.aux
+rm *.dvi
+rm *.log
+rm *.nav
+rm *.out
+rm *.snm
+rm *.toc
